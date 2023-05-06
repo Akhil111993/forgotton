956e7e6d32bb836b3b9ac407a1f913a0567ec95d
AKHIL V S <akhilv.reubro@gmail.com>
2023-05-07 02:52:29 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+import 'ember_quest.dart';
+import 'overlays/game_over.dart';
+import 'overlays/main_menu.dart';
+
+void main() {
+  final game = FlameGame();
+   runApp(
+    GameWidget<EmberQuestGame>.controlled(
+      gameFactory: EmberQuestGame.new,
+      overlayBuilderMap: {
+        'MainMenu': (_, game) => MainMenu(game: game),
+        'GameOver': (_, game) => GameOver(game: game),
+      },
+      initialActiveOverlays: const ['MainMenu'],
+    ),
+  );
+}
+
